544987386324e4ffb116ec3961d3e9bc5ea37cfe
Usufjon <yusufjonov98@gmail.com>
2019-09-28 00:15:53 +0700

    [+] Getting all words

--- scr.py
+++ scr.py
@@ -1,4 +1,4 @@
-import requests
+import requests, random
 from requests.auth import HTTPBasicAuth
 
 
@@ -17,8 +17,52 @@ def getLinksTo100Words(url):
         result2.append("http://vocabulary.englishprofile.org" + i)
     return result2
 
-links = getLinksTo100Words("http://vocabulary.englishprofile.org/dictionary/search/uk/?pageSize=100&q=&wl=301&p=1")
+def getWord(url):
+    page = requests.get(url, auth=('englishprofile', 'vocabulary'))
+    result = page.text
+    
+    word = result.split("""<h1 class="hw" resource="us" query="yes">""")[1].split("</h1")[0]
+    defenition = result.split("""<span class="def" resource="us" query="yes"><span> </span>""")[1].split("</span>")[0]
+     
+    pair = {word: defenition}
+    print(pair)
+    return pair
+ 
+def getListOfWordsfromLink(links):
+    words = []
+    for i in links:
+        words.append(getWord(i))
+    return words
 
+def getlinks(url):
+    result = []
+    for i in range(1,9):
+        links = getLinksTo100Words(url + str(i))    
+        links.pop(0)
+        result += links
+    return result
+
+def saveToFile(words):
+    f = open("a1_us.txt", 'w', encoding='utf8')
+    for i in words:
+        f.write(list(i.items())[0][0])
+        f.write(" - ")
+        f.write(list(i.items())[0][1])
+        # print(i)
+        # f.write(str(i))
+        f.write("\n")
+    f.close()
+
+
+links = getlinks("""http://vocabulary.englishprofile.org/dictionary/search/us/?pageSize=100&q=&wl=301&p=""")
+words = getListOfWordsfromLink(links)
+
+random.shuffle(words)
+random.shuffle(words)
+            
+saveToFile(words)
+    
+    
 
 
 
